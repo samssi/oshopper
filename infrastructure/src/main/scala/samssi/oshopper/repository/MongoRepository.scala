8ad3e281834578bd3ab1be63a983ef80dac985b3
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-17 17:25:13 +0200

    MongoDB uri from system param

--- infrastructure/src/main/scala/samssi/oshopper/repository/MongoRepository.scala
+++ infrastructure/src/main/scala/samssi/oshopper/repository/MongoRepository.scala
@@ -5,7 +5,8 @@ import com.mongodb.casbah.MongoClientURI
 import com.mongodb.util.JSON._
 
 class MongoRepository {
-  lazy val centralClient = MongoClient(MongoClientURI("mongodb://xxx:xxx@xxx.mongolab.com:53958/central"))
+  lazy val mongoUri = PropertiesUtil.getMongoUri
+  lazy val centralClient = MongoClient(MongoClientURI(mongoUri))
   def insert(product: String) {
     val products = centralClient("central").getCollection("products")
     val document = parse(product).asInstanceOf[DBObject]
