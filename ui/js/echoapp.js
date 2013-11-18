d35d5b4460428d6bfd1b580ceb7f914ebfd2f619
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-18 18:08:30 +0200

    Cleanup

--- ui/js/echoapp.js
+++ ui/js/echoapp.js
@@ -9,14 +9,3 @@ function echoController($scope, Restangular) {
         });
     }
 }
-
-/*function echoController($scope, Restangular) {
-    $scope.init = function() {
-        $http.get("http://localhost:8080/oshopper/v1/echo/ping")
-            .success(function(data, status, headers, config) {
-                $scope.apiResponse = data.message;
-            }).error(function(data, status, headers, config) {
-                $scope.apiResponse = "No response from the API!"
-            });
-    }
-}*/
