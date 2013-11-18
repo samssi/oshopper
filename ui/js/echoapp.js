685e32a197bc82dde8f173d44627a6eb93263a45
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-18 18:07:45 +0200

    Start using Restangular

--- ui/js/echoapp.js
+++ ui/js/echoapp.js
@@ -1,6 +1,16 @@
-var echo = angular.module('echo', []);
+var echo = angular.module('echo',  ['restangular']);
 
-function echoController($scope, $http) {
+
+
+function echoController($scope, Restangular) {
+    $scope.init = function() {
+        Restangular.oneUrl('apiResponse', 'http://localhost:8080/oshopper/v1/echo/ping').get().then(function(apiResponse) {
+            $scope.apiResponse = apiResponse.message
+        });
+    }
+}
+
+/*function echoController($scope, Restangular) {
     $scope.init = function() {
         $http.get("http://localhost:8080/oshopper/v1/echo/ping")
             .success(function(data, status, headers, config) {
@@ -9,4 +19,4 @@ function echoController($scope, $http) {
                 $scope.apiResponse = "No response from the API!"
             });
     }
-}
+}*/
