4f30ebe66e488375ce36302df50ad581fc23c97c
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-19 13:48:43 +0200

    Refactor

--- infrastructure/src/main/scala/samssi/oshopper/repository/ProductRepository.scala
+++ infrastructure/src/main/scala/samssi/oshopper/repository/ProductRepository.scala
@@ -8,18 +8,18 @@ import org.bson.types.ObjectId
 trait CentralRepository {
   lazy val mongoUri = PropertiesUtil.getMongoUri
   lazy val centralClient = MongoClient(MongoClientURI(mongoUri))
-  def collection(collection: String) = centralClient("central").getCollection(collection)
+  def repositoryCollection: String
+  def fetchCollectionFromDb(collection: String) = centralClient("central").getCollection(collection)
+  def insert(json: String) { fetchCollectionFromDb(repositoryCollection).insert(parse(json).asInstanceOf[DBObject]) }
+  def select(id: String) = fetchCollectionFromDb(repositoryCollection).findOne(ObjectId.massageToObjectId(id)).toString
+  def delete(id: String) = fetchCollectionFromDb(repositoryCollection).remove(DBObject("_id" -> ObjectId.massageToObjectId(id)))
 }
 
 class ProductRepository extends CentralRepository {
-  def insert(json: String) { collection("products").insert(parse(json).asInstanceOf[DBObject]) }
-  def select(id: String) = collection("products").findOne(ObjectId.massageToObjectId(id)).toString
-  def delete(id: String) = collection("products").remove(DBObject("_id" -> ObjectId.massageToObjectId(id)))
-  def getCategories = collection("products").distinct("category").toString
+  def repositoryCollection = "products"
+  def getCategories = fetchCollectionFromDb("products").distinct("category").toString
 }
 
 class CustomerRepository extends CentralRepository {
-  def insert(json: String) { collection("customers").insert(parse(json).asInstanceOf[DBObject]) }
-  def select(id: String) = collection("customers").findOne(ObjectId.massageToObjectId(id)).toString
-  def delete(id: String) = collection("customers").remove(DBObject("_id" -> ObjectId.massageToObjectId(id)))
+  def repositoryCollection = "customers"
 }
