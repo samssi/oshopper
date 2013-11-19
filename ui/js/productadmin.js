8dbb07ea06635f27405bb2b2fc35a5afd5640172
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-19 12:25:16 +0200

    Cleanup

--- ui/js/productadmin.js
+++ ui/js/productadmin.js
@@ -3,14 +3,5 @@ var productadmin = angular.module('productadmin', ['restangular']);
 function productController($scope, Restangular) {
     $scope.add = function() {
         Restangular.oneUrl('root', 'http://localhost:8080/oshopper/v1/').post('products', $scope.product);
-        console.log($scope.product);
     }
-    /*$scope.init = function() {
-        $http.post("http://localhost:8080/oshopper/v1/products", json)
-            .success(function(data, status, headers, config) {
-                console.log(status);
-            }).error(function(data, status, headers, config) {
-                console.log("No response from the API!")
-            });
-    }*/
 }
