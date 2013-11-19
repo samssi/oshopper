fb76a03efe60050480bf55d57ff43e9b35380672
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-19 12:02:07 +0200

    Restangular echo

--- application/src/main/scala/samssi/oshopper/servlet/EchoResource.scala
+++ application/src/main/scala/samssi/oshopper/servlet/EchoResource.scala
@@ -5,6 +5,7 @@ import samssi.oshopper.domain.Echo
 
 class EchoResource extends OshopperServletSupport {
   get("/ping") {
+    println("Received ping with following params: " + params)
     Ok(Echo("pong"))
   }
 }
--- ui/js/echoapp.js
+++ ui/js/echoapp.js
@@ -1,7 +1,5 @@
 var echo = angular.module('echo',  ['restangular']);
 
-
-
 function echoController($scope, Restangular) {
     $scope.init = function() {
         Restangular.oneUrl('apiResponse', 'http://localhost:8080/oshopper/v1/echo/ping').get().then(function(apiResponse) {
