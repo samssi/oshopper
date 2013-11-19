5434482ba3e810cdd92d2a7a242921e804f7d7ed
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-19 14:46:20 +0200

    Delete now unused .js files

--- ui/js/customeradmin.js
+++
@@ -1,7 +0,0 @@
-var customeradmin = angular.module('customeradmin', ['restangular']);
-
-function customerController($scope, Restangular) {
-    $scope.add = function() {
-        Restangular.oneUrl('root', 'http://localhost:8080/oshopper/v1/').post('customers', $scope.customer);
-    }
-}
--- ui/js/productadmin.js
+++
@@ -1,7 +0,0 @@
-var productadmin = angular.module('productadmin', ['restangular']);
-
-function productController($scope, Restangular) {
-    $scope.add = function() {
-        Restangular.oneUrl('root', 'http://localhost:8080/oshopper/v1/').post('products', $scope.product);
-    }
-}
