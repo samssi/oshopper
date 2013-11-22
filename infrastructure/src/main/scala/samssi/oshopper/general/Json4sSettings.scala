c5d249d895c592c7b57659662d6233f0237d7cd4
Sämir Hamadi <samir.hamadi@elisa.fi>
2013-11-22 18:44:52 +0200

    Cleanup

--- infrastructure/src/main/scala/samssi/oshopper/general/Json4sSettings.scala
+++ infrastructure/src/main/scala/samssi/oshopper/general/Json4sSettings.scala
@@ -1,14 +1,7 @@
 package samssi.oshopper.general
 
 import org.json4s.DefaultFormats
-import org.json4s.native.JsonMethods._
 
 class Json4sSettings {
   implicit val jsonFormats = DefaultFormats
-
-  def parseWithId(json: String) = {
-    parse(json).transformField {
-      case("_id", x) => ("id", x \ "$oid")
-    }
-  }
 }
