cd808eea53f8fe645a00aa7cbbc96bf623b75ae1
samssi <samssi@samssi.(none)>
2014-03-29 23:54:03 +0200

    Product selection

--- ui/js/customer.js
+++ ui/js/customer.js
@@ -10,8 +10,10 @@ customer.controller('searchPanelController', function($scope, Restangular) {
     var position = -1;
     $scope.searchForProduct = function($event) {
         console.log($event);
+
         var searchWord = $scope.searchword;
         var escKeyCode = 27;
+
         if ($event.keyCode == escKeyCode) {
             $scope.resetSearch();
         }
@@ -26,8 +28,11 @@ customer.controller('searchPanelController', function($scope, Restangular) {
         else if ($event.keyCode == escKeyCode) {
             $scope.resetSearch();
         }
+        // Enter
+        else if ($event.keyCode == 13) {
+            alert($scope.searchedProducts[position].name)
+        }
         else if (searchWord.length > 3) {
-            // Make into service
             console.log('Searching for product starting with: ' + searchWord);
             Restangular.one('products').get({searchword: searchWord}).then(function(productList) {
                 $scope.searchedProducts = productList;
@@ -50,7 +55,7 @@ customer.controller('searchPanelController', function($scope, Restangular) {
             position = position - 1;
             //console.log(angular.element($scope.searchedProducts[position].id))
             //console.log(angular.element.find($scope.searchedProducts[position].id))
-            document.getElementById($scope.searchedProducts[position].id).className = 'selected';
+            //document.getElementById($scope.searchedProducts[position].id).className = 'selected';
             console.log('position: ' + position + " id: " + $scope.searchedProducts[position].id)
         }
     }
